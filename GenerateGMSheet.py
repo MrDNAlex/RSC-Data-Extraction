b9f446f550b0df4c5606e307c90e67bc67ee50d5
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2025-03-10 00:05:09 -0400

    Fully functional single script

--- GenerateGMSheet.py
+++ GenerateGMSheet.py
@@ -1,6 +1,390 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import os
+from openpyxl import load_workbook, Workbook
+from openpyxl.styles import Alignment, Font, PatternFill, Border, Side
+from openpyxl.worksheet.table import Table, TableStyleInfo
+from openpyxl.utils import get_column_letter
+
+SchedulePath = "Input/RSC_Schedule.xlsx"
+RPVPath = "Input/RSC_RPV.xlsx"
+SBVPath = "Input/RSC_SBV.xlsx"
+IDRPath = "Input/RSC_IDR.xlsx"
+
+# Check if all Stats Files Exist
+if not os.path.exists(SchedulePath):
+    print(
+        "Schedule File is not Found. Please place it in the Input folder under the name 'RSC_Schedule.xlsx'"
+    )
+    exit()
+
+if not os.path.exists(RPVPath):
+    print(
+        "RPV File is not Found. Please place it in the Input folder under the name 'RSC_RPV.xlsx'"
+    )
+    exit()
+
+if not os.path.exists(SBVPath):
+    print(
+        "SBV File is not Found. Please place it in the Input folder under the name 'RSC_SBV.xlsx'"
+    )
+    exit()
+
+if not os.path.exists(IDRPath):
+    print(
+        "IDR File is not Found. Please place it in the Input folder under the name 'RSC_IDR.xlsx'"
+    )
+    exit()
+
+print("Loading Excel Sheets into Memory...")
+
+AtoL = 12 # 12 Columns
+TeamsPerRow = AtoL - 3
+
+# Load the Excel Sheets
+ScheduleSheet = pd.ExcelFile(SchedulePath)
+RPVSheet = pd.ExcelFile(RPVPath)
+SBVSheet = pd.ExcelFile(SBVPath)
+IDRSheet = pd.ExcelFile(IDRPath)
+
+print("Excel Sheets Loaded Successfully!")
+
+def GetTable(sheet: pd.ExcelFile, sheetName: str, skipRows: int, useCols: str, nRows: int) -> pd.DataFrame:
+    return sheet.parse(
+        sheetName,
+        skiprows=skipRows,
+        usecols=useCols,
+        nrows=nRows,
+    )
+
+# def LinearizeMatchDaySchedule (sheet, preseasonDays, matchDays,  playoffDays, spacing, hasLunarConference=True):
+def LinearizeMatchDaySchedule (scheduleSheet,  tierInfo):
+    
+    sheet = GetTable(scheduleSheet, tierInfo["matchSheetName"], tierInfo["skiprowsMatch"], tierInfo["usecolsMatch"], tierInfo["nrowsMatch"])
+    
+    preseasonDays = tierInfo["preseasonDays"]
+    matchDays = tierInfo["matchDays"]
+    playoffDays = tierInfo["playoffDays"]
+    spacing = tierInfo["spacing"]
+    hasLunarConference = tierInfo["hasLunarConference"]
+    
+    TotalDays = preseasonDays + matchDays
+    
+    OfficialSchedule = sheet.iloc[:TotalDays, [0, -1]]
+    OfficialSchedule.columns = ["Match Day", "Date"]
+    
+    SolarConference = sheet.iloc[:TotalDays, 1:-1]
+    SolarConference = SolarConference.dropna(axis=1, how='all')
+    
+    # Fill in Playoff Days
+    for i in range(TotalDays - playoffDays, TotalDays):
+        DayValue = SolarConference.iloc[i, 0]
+        SolarConference.iloc[i, :] = DayValue
+        
+    OfficialSchedule = pd.concat([OfficialSchedule, SolarConference], axis=1)
+
+    if hasLunarConference:
+        LunarConferenceTeamNames = sheet.iloc[TotalDays + spacing, 1:-1].values
+        LunarConference = sheet.iloc[TotalDays + spacing+1:TotalDays + spacing + TotalDays + 1, 1:-1]
+        LunarConference.columns = LunarConferenceTeamNames
+        LunarConference.reset_index(drop=True, inplace=True)
+        LunarConference = LunarConference.dropna(axis=1, how='all')
+        
+        # Fill in Playoff Days
+        for i in range(TotalDays - playoffDays, TotalDays):
+            DayValue = LunarConference.iloc[i, 0]
+            LunarConference.iloc[i, :] = DayValue
+            
+        OfficialSchedule = pd.concat([OfficialSchedule, LunarConference], axis=1)
+        OfficialSchedule = OfficialSchedule.dropna(axis=1, how='all')
+
+    return OfficialSchedule
+
+def LinearizeScrimDaySchedule (scheduleSheet, tierInfo):
+    
+    sheet = GetTable(scheduleSheet, tierInfo["scrimSheetName"], tierInfo["skiprowsScrim"], tierInfo["usecolsScrim"], tierInfo["nrowsScrim"])
+    
+    numOfRows = tierInfo["scrimRows"]
+    matchDays = tierInfo["matchDaysScrim"]
+    spacing = tierInfo["spacingScrim"]
+    
+    OfficialSheet = sheet.iloc[:matchDays, [0, 2]]
+    FirstRow = sheet.iloc[:matchDays, 3:]
+    OfficialSheet = pd.concat([OfficialSheet, FirstRow], axis=1)
+    
+    Start = matchDays + spacing
+    
+    for i in range(1, numOfRows):
+        RowNames = sheet.iloc[Start][2:].values
+        Row = sheet.iloc[(Start + 1):(Start + 1) + matchDays, 2:]
+        Row.columns = RowNames
+        Row.reset_index(drop=True, inplace=True)
+        Row.dropna(axis=1, how='all')
+        Row.dropna(axis=0, how='all')
+        
+        Start += (matchDays + spacing) 
+        
+        OfficialSheet = pd.concat([OfficialSheet, Row], axis=1)
+    
+    print(OfficialSheet)
+    OfficialSheet = OfficialSheet.loc[:, ~OfficialSheet.columns.isna()]
+    ScrimNumbers = len(OfficialSheet["Game"].values)
+    OfficialSheet["Game"] =  ["Match Day " + str(i) for i in range(1, ScrimNumbers + 1)]
+    OfficialSheet.rename(columns={"Game": "Match Day"}, inplace=True)
+    print(OfficialSheet)
+    
+    return OfficialSheet
+
+def GetStatsSheet (statsInfo):
+    
+    RPVTable = GetTable(statsInfo[0]["sheet"], statsInfo[0]["sheetName"], statsInfo[0]["skiprows"], statsInfo[0]["usecols"], statsInfo[0]["nrows"])
+    SBVTable = GetTable(statsInfo[1]["sheet"], statsInfo[1]["sheetName"], statsInfo[1]["skiprows"], statsInfo[1]["usecols"], statsInfo[1]["nrows"])
+    IDRTable = GetTable(statsInfo[2]["sheet"], statsInfo[2]["sheetName"], statsInfo[2]["skiprows"], statsInfo[2]["usecols"], statsInfo[2]["nrows"])
+    
+    IDRTable.rename(columns={"Team": "Current Team"}, inplace=True)
+    mergedTable = (
+            RPVTable
+            .merge(SBVTable, on=["Name"])
+            .merge(IDRTable, on=["Name"])
+            )
+    
+    mergedTable = mergedTable.rename(columns={col: col.replace("_x", "") for col in mergedTable.columns if "_x" in col})
+    mergedTable = mergedTable.rename(columns={col: col.replace("(s)", "_y") for col in mergedTable.columns if "(s)" in col})
+
+    # Remove columns containing "_y" or "_z"
+    remove_cols = [col for col in mergedTable.columns if "_y" in col or "_z" in col]
+    mergedTable = mergedTable.drop(columns=remove_cols)
+    
+    mergedTable = mergedTable.loc[:, ~mergedTable.columns.duplicated()]
+    
+    return mergedTable
+    
+def GetExcelRange(start_row, start_col, df):
+    rows, cols = df.shape
+    end_row = start_row + rows  # Bottom-right row
+    end_col = start_col + cols - 1  # Bottom-right column (adjust for zero indexing)
+    
+    
+    start_cell = f"{get_column_letter(start_col)}{start_row}"
+    end_cell = f"{get_column_letter(end_col)}{end_row}" # Index = False, Headers = True, adjust based off that
+    
+    return f"{start_cell}:{end_cell}"
+
+def PasteDataFrame(dataFrame, workSheet, startRow = 1, startCol = 1):
+    for j, col_name in enumerate(dataFrame.columns, start=startCol):  # Write column headers
+        workSheet.cell(row=startRow, column=j).value = col_name
+    
+    for row_num, row_data in enumerate(dataFrame.itertuples(index=False), start=startRow + 1):  # Write data
+        for col_num, value in enumerate(row_data, start=startCol):
+            workSheet.cell(row=row_num, column=col_num).value = value
+
+#def PasteDataFrame(dataFrame, workSheet, startRow=1, startCol=1):
+#    for j, col_name in enumerate(dataFrame.columns, start=startCol):  # Write column headers
+#        cell = workSheet.cell(row=startRow, column=j)
+#        
+#        # Skip if merged cell
+#        if cell.coordinate in workSheet.merged_cells:
+#            print(f"Skipping merged cell: {cell.coordinate}")
+#            continue
+#        
+#        cell.value = col_name
+#
+#    for row_num, row_data in enumerate(dataFrame.itertuples(index=False), start=startRow + 1):
+#        for col_num, value in enumerate(row_data, start=startCol):
+#            cell = workSheet.cell(row=row_num, column=col_num)
+#            
+#            # Skip if merged cell
+#            if cell.coordinate in workSheet.merged_cells:
+#                print(f"Skipping merged cell: {cell.coordinate}")
+#                continue
+#            
+#            cell.value = value
+
+def AddDataFrameAsTable (dataFrame, workSheet, tableName, startRow = 1, startCol = 1):
+    PasteDataFrame(dataFrame, workSheet, startRow, startCol)
+
+    if (tableName not in workSheet.tables):
+        workSheet.add_table(Table(displayName=tableName, ref=GetExcelRange(startRow, startCol, dataFrame)))
 
 
-ScheduleSheet = pd.ExcelFile('./Input/Match Day 10')
+#def GetTierTables (sheetInfo):
+#    sheet = GetTable(sheetInfo["sheet"], sheetInfo["sheetName"], sheetInfo["skiprows"], sheetInfo["usecols"], sheetInfo["nrows"])
+#
+#    TierTables = []
+#
+#    for tier in Tiers:
+#        tierTable = sheet[sheet["Tier"] == tier]
+#        tierTable.reset_index(drop=True, inplace=True)
+#        tierTable.columns = tierTable.columns.str.strip()
+#        TierTables.append(tierTable)
+#        
+#    return TierTables
+#
+#def GetStatsSheet (statsInfo):
+#    RPVTables = GetTierTables(statsInfo[0])
+#    SBVTables = GetTierTables(statsInfo[1])
+#    IDRTables = GetTierTables(statsInfo[2])
+#    
+#    MergedTables = []
+#    
+#    for i in range(len(Tiers)):
+#        
+#        IDRTables[i].rename(columns={"Team": "Current Team"}, inplace=True)
+#        
+#        mergedTable = (
+#            RPVTables[i]
+#            .merge(SBVTables[i], on=["Name"])
+#            .merge(IDRTables[i], on=["Name"])
+#            )
+#        
+#        mergedTable = mergedTable.rename(columns={col: col.replace("_x", "") for col in mergedTable.columns if "_x" in col})
+#        mergedTable = mergedTable.rename(columns={col: col.replace("(s)", "_y") for col in mergedTable.columns if "(s)" in col})
+#
+#        # Remove columns containing "_y" or "_z"
+#        remove_cols = [col for col in mergedTable.columns if "_y" in col or "_z" in col]
+#        mergedTable = mergedTable.drop(columns=remove_cols)
+#        
+#        mergedTable = mergedTable.loc[:, ~mergedTable.columns.duplicated()]
+#        
+#        MergedTables.append(mergedTable)
+#    
+#    StatsInfo = pd.concat(MergedTables, keys=Tiers)
+#    
+#    return StatsInfo
 
+Tiers = [
+    "Premier",
+    "Master",
+    "Elite",
+    "Veteran",
+    "Rival",
+    "Challenger",
+    "Prospect",
+    "Contender",
+    "Amateur",
+]
+
+schedulePages = ScheduleSheet.sheet_names
+
+MatchDayScheduleSheetName = schedulePages[0]
+
+print("Extracting Match Day Schedules...")
+
+# Master Scrim is one shorter than reported
+
+TierInfo = []
+TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[1], "skiprowsMatch": 5, "usecolsMatch": "B:I", "nrowsMatch": 49 - 6,       "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 49-14, "preseasonDays": 3, "matchDays": 16, "playoffDays": 2, "spacing": 4, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
+TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[2], "skiprowsMatch": 56, "usecolsMatch": "B:K", "nrowsMatch": 102 - 57,    "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 49-14,  "preseasonDays": 3,"matchDays": 17, "playoffDays": 2,"spacing": 4,  "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
+TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[3], "skiprowsMatch": 109, "usecolsMatch": "B:Q", "nrowsMatch": 158 - 110,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 87-14, "preseasonDays": 3, "matchDays": 18, "playoffDays": 3, "spacing": 5, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
+TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[4], "skiprowsMatch": 167, "usecolsMatch": "B:V", "nrowsMatch": 218 - 168,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 87-14, "preseasonDays": 3, "matchDays": 19, "playoffDays": 3, "spacing": 5, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": True})
+TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[5], "skiprowsMatch": 227, "usecolsMatch": "B:V", "nrowsMatch": 278 - 228,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 87-14, "preseasonDays": 3, "matchDays": 19, "playoffDays": 3, "spacing": 5, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": True})
+TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[6], "skiprowsMatch": 285, "usecolsMatch": "B:Q", "nrowsMatch": 334 - 286,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 87-14, "preseasonDays": 3, "matchDays": 18, "playoffDays": 3, "spacing": 5, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
+TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[7], "skiprowsMatch": 341, "usecolsMatch": "B:K", "nrowsMatch": 387 - 342,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 49-14, "preseasonDays": 3, "matchDays": 17, "playoffDays": 2, "spacing": 4, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
+TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[8], "skiprowsMatch": 394, "usecolsMatch": "B:H", "nrowsMatch": 438 - 395,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 49-14, "preseasonDays": 3, "matchDays": 16, "playoffDays": 3, "spacing": 4, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
+TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[9], "skiprowsMatch": 445, "usecolsMatch": "B:K", "nrowsMatch": 464 - 446,  "skiprowsScrim": 13, "usecolsScrim": "B:k", "nrowsScrim": 30-14, "preseasonDays": 3, "matchDays": 15, "playoffDays": 1, "spacing": 4, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": False, "hasDivisions": False})
+
+StatsInfo = []
+StatsInfo.append({"sheet" : RPVSheet, "sheetName": "Sorted RPV", "skiprows": 4, "usecols": "A:AD", "nrows": 848})
+StatsInfo.append({"sheet" : SBVSheet,"sheetName": "Sorted Player Stats", "skiprows": 0, "usecols": "D:AQ", "nrows": 848})
+StatsInfo.append({"sheet" : IDRSheet,"sheetName": "IDR View", "skiprows": 4, "usecols": "A:AQ", "nrows": 848})
+
+TeamsInfoCoords = { "skiprows": 0, "usecols": "D:AM", "nrows": 183 }
+
+MatchScheduleSheets = {}
+ScrimScheduleSheets = {}
+
+for i in range(len(Tiers)):
+    MatchScheduleSheets[Tiers[i]] = LinearizeMatchDaySchedule(ScheduleSheet, TierInfo[i])
+    
+    divisionOffset = 3 if TierInfo[i]["hasDivisions"] else 0
+    conferenceMultiple = 2 if TierInfo[i]["hasLunarConference"] else 1
+    
+    teamColumnNums = len(GetTable(ScheduleSheet, TierInfo[i]["matchSheetName"], TierInfo[i]["skiprowsMatch"], TierInfo[i]["usecolsMatch"], TierInfo[i]["nrowsMatch"]).columns)
+    
+    TierInfo[i]["teamNum"] = (teamColumnNums - 2 - divisionOffset) * conferenceMultiple
+    TierInfo[i]["scrimRows"] = int(TierInfo[i]["teamNum"] / TeamsPerRow) + 1
+    
+    ScrimScheduleSheets[Tiers[i]] = LinearizeScrimDaySchedule(ScheduleSheet, TierInfo[i])
+
+print("Match Day Schedules Extracted Successfully!")
+
+print("Extracting Stats Data...")
+
+StatsSheet = GetStatsSheet(StatsInfo)
+
+print("Stats Data Extracted Successfully!")
+
+# Extract all Teams Info
+print("Extracting Teams Info...")
+
+TeamInfoTable = SBVSheet.parse("Sorted Team Stats", skiprows=TeamsInfoCoords["skiprows"], usecols=TeamsInfoCoords["usecols"], nrows=TeamsInfoCoords["nrows"])
+
+print("Teams Info Extracted Successfully!")
+
+print("Generating GM Excel Sheet...")
+
+WorkBook = load_workbook("Template/RSC_Info_Template.xlsx")
+
+if ("Schedules" not in WorkBook.sheetnames):
+    WorkBook.create_sheet("Schedules")
+WorkSheet = WorkBook["Schedules"]
+
+rowIndex = 1
+
+for i in range(len(Tiers)):
+    print("Tiers : " + Tiers[i])
+    
+    print("Row Index : " + str(rowIndex))
+    
+    WorkSheet.merge_cells(start_row=rowIndex, start_column=1, end_row=rowIndex + 1, end_column=MatchScheduleSheets[Tiers[i]].columns.size)
+    WorkSheet.cell(row=rowIndex, column=1).value = Tiers[i] + " Match Schedule"
+    WorkSheet.cell(row=rowIndex, column=1).alignment = Alignment(horizontal='center', vertical='center')
+
+    rowIndex += 2
+    print("Row Index : " + str(rowIndex))
+    
+    # Paste Match Schedule DataFrame
+    AddDataFrameAsTable(MatchScheduleSheets[Tiers[i]], WorkSheet, Tiers[i] + "MatchSchedule", startRow=rowIndex)
+    
+    rowIndex += MatchScheduleSheets[Tiers[i]].shape[0] + 1
+    print("Row Index : " + str(rowIndex))
+    
+    WorkSheet.merge_cells(start_row=rowIndex, start_column=1, end_row=rowIndex, end_column=MatchScheduleSheets[Tiers[i]].columns.size)
+    WorkSheet.cell(row=rowIndex, column=1).fill = PatternFill(start_color="000000", end_color="000000", fill_type="solid")
+    
+    rowIndex += 1
+    print("Row Index : " + str(rowIndex))
+    
+    # Scrim
+    WorkSheet.merge_cells(start_row=rowIndex, start_column=1, end_row=rowIndex + 1, end_column=ScrimScheduleSheets[Tiers[i]].columns.size)
+    WorkSheet.cell(row=rowIndex, column=1).value = Tiers[i] + " Scrim Schedule"
+    WorkSheet.cell(row=rowIndex, column=1).alignment = Alignment(horizontal='center', vertical='center')
+    
+    rowIndex += 2
+    print("Row Index : " + str(rowIndex))
+    
+    AddDataFrameAsTable(ScrimScheduleSheets[Tiers[i]], WorkSheet, Tiers[i] + "ScrimSchedule", startRow=rowIndex)
+    
+    rowIndex += ScrimScheduleSheets[Tiers[i]].shape[0] + 1
+    print("Row Index : " + str(rowIndex))
+    
+    WorkSheet.merge_cells(start_row=rowIndex, start_column=1, end_row=rowIndex, end_column=ScrimScheduleSheets[Tiers[i]].columns.size)
+    WorkSheet.cell(row=rowIndex, column=1).fill = PatternFill(start_color="000000", end_color="000000", fill_type="solid")
+    
+    rowIndex += 1
+    print("Row Index : " + str(rowIndex))
+
+if ("Player Stats" not in WorkBook.sheetnames):
+    WorkBook.create_sheet("Player Stats")
+WorkSheet = WorkBook["Player Stats"]
+AddDataFrameAsTable(StatsSheet, WorkSheet, "PlayerStats")
+
+if ("Team Stats" not in WorkBook.sheetnames):
+    WorkBook.create_sheet("Team Stats")
+WorkSheet = WorkBook["Team Stats"]
+AddDataFrameAsTable(TeamInfoTable, WorkSheet, "TeamStats")
+
+WorkBook.save("RSC_Info.xlsx")
+
+print("GM Excel Sheet Generated Successfully!")
