e6cdbbeae3edb0f42e642c76ade20f3aa0f87055
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2025-06-18 22:54:17 -0400

    Fixed Obsidian Exporter

--- GenerateObsidianPlayerFiles.py
+++ GenerateObsidianPlayerFiles.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import os
-from openpyxl import load_workbook
-from openpyxl.styles import Alignment, PatternFill
 from openpyxl.worksheet.table import Table
 from openpyxl.utils import get_column_letter
 import re
 
-SchedulePath = "Input/RSC_Schedule.xlsx"
-RPVPath = "Input/RSC_RPV.xlsx"
-SBVPath = "Input/RSC_SBV.xlsx"
-IDRPath = "Input/RSC_IDR.xlsx"
+SchedulePath = "InputS23/RSC_Schedule.xlsx"
+RPVPath = "InputS23/RSC_RPV.xlsx"
+SBVPath = "InputS23/RSC_SBV.xlsx"
+IDRPath = "InputS23/RSC_IDR.xlsx"
 
 # Check if all Stats Files Exist
 if not os.path.exists(SchedulePath):
@@ -38,8 +36,8 @@ if not os.path.exists(IDRPath):
 
 print("Loading Excel Sheets into Memory...")
 
-AtoL = 12 # 12 Columns
-TeamsPerRow = AtoL - 3
+#AtoL = 12 # 12 Columns
+#TeamsPerRow = AtoL - 3
 
 # Load the Excel Sheets
 ScheduleSheet = pd.ExcelFile(SchedulePath)
@@ -47,7 +45,67 @@ RPVSheet = pd.ExcelFile(RPVPath)
 SBVSheet = pd.ExcelFile(SBVPath)
 IDRSheet = pd.ExcelFile(IDRPath)
 
+Tiers = [
+    "Premier",
+    "Master",
+    "Elite",
+    "Veteran",
+    "Rival",
+    "Challenger",
+    "Prospect",
+    "Contender",
+    "Amateur",
+]
+
+schedulePages = ScheduleSheet.sheet_names
+
+MatchDayScheduleSheetName = schedulePages[0]
+
+print("Extracting Match Day Schedules...")
+
+#TierInfo = []
+#TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[1], "skiprowsMatch": 5, "usecolsMatch": "B:I", "nrowsMatch": 49 - 6,       "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 49-14, "preseasonDays": 3, "matchDays": 16, "playoffDays": 2, "spacing": 4, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
+#TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[2], "skiprowsMatch": 56, "usecolsMatch": "B:K", "nrowsMatch": 102 - 57,    "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 49-14,  "preseasonDays": 3,"matchDays": 17, "playoffDays": 2,"spacing": 4,  "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
+#TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[3], "skiprowsMatch": 109, "usecolsMatch": "B:Q", "nrowsMatch": 158 - 110,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 87-14, "preseasonDays": 3, "matchDays": 18, "playoffDays": 3, "spacing": 5, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
+#TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[4], "skiprowsMatch": 167, "usecolsMatch": "B:V", "nrowsMatch": 218 - 168,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 87-14, "preseasonDays": 3, "matchDays": 19, "playoffDays": 3, "spacing": 5, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": True})
+#TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[5], "skiprowsMatch": 227, "usecolsMatch": "B:V", "nrowsMatch": 278 - 228,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 87-14, "preseasonDays": 3, "matchDays": 19, "playoffDays": 3, "spacing": 5, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": True})
+#TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[6], "skiprowsMatch": 285, "usecolsMatch": "B:Q", "nrowsMatch": 334 - 286,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 87-14, "preseasonDays": 3, "matchDays": 18, "playoffDays": 3, "spacing": 5, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
+#TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[7], "skiprowsMatch": 341, "usecolsMatch": "B:K", "nrowsMatch": 387 - 342,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 49-14, "preseasonDays": 3, "matchDays": 17, "playoffDays": 2, "spacing": 4, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
+#TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[8], "skiprowsMatch": 394, "usecolsMatch": "B:H", "nrowsMatch": 438 - 395,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 49-14, "preseasonDays": 3, "matchDays": 16, "playoffDays": 3, "spacing": 4, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
+#TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[9], "skiprowsMatch": 445, "usecolsMatch": "B:K", "nrowsMatch": 464 - 446,  "skiprowsScrim": 13, "usecolsScrim": "B:k", "nrowsScrim": 30-14, "preseasonDays": 3, "matchDays": 15, "playoffDays": 1, "spacing": 4, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": False, "hasDivisions": False})
+
+NumberOfPlayers = 841
+
+StatsInfo = []
+StatsInfo.append({"sheet" : RPVSheet, "sheetName": "Sorted RPV", "skiprows": 4, "usecols": "A:AD", "nrows": NumberOfPlayers})
+StatsInfo.append({"sheet" : SBVSheet,"sheetName": "Sorted Player Stats", "skiprows": 0, "usecols": "A:AR", "nrows": NumberOfPlayers})
+StatsInfo.append({"sheet" : IDRSheet,"sheetName": "IDR View", "skiprows": 4, "usecols": "A:AQ", "nrows": NumberOfPlayers})
+
+TeamsInfoCoords = { "sheet" : SBVSheet, "sheetName" : "Sorted Team Stats", "skiprows": 0, "usecols": "A:AK", "nrows": 177 }
+
+MatchScheduleSheets = {}
+ScrimScheduleSheets = {}
+
 print("Excel Sheets Loaded Successfully!")
+
+
+#for i in range(len(Tiers)):
+#    MatchScheduleSheets[Tiers[i]] = LinearizeMatchDaySchedule(ScheduleSheet, TierInfo[i])
+#    
+#    divisionOffset = 3 if TierInfo[i]["hasDivisions"] else 0
+#    conferenceMultiple = 2 if TierInfo[i]["hasLunarConference"] else 1
+#    
+#    teamColumnNums = len(GetTable(ScheduleSheet, TierInfo[i]["matchSheetName"], TierInfo[i]["skiprowsMatch"], TierInfo[i]["usecolsMatch"], TierInfo[i]["nrowsMatch"]).columns)
+#    
+#    TierInfo[i]["teamNum"] = (teamColumnNums - 2 - divisionOffset) * conferenceMultiple
+#    TierInfo[i]["scrimRows"] = int(TierInfo[i]["teamNum"] / TeamsPerRow) + 1
+#    
+#    ScrimScheduleSheets[Tiers[i]] = LinearizeScrimDaySchedule(ScheduleSheet, TierInfo[i])
+
+
+#
+# Function Definitions
+#
 
 def GetTable(sheet: pd.ExcelFile, sheetName: str, skipRows: int, useCols: str, nRows: int) -> pd.DataFrame:
     return sheet.parse(
@@ -138,7 +196,9 @@ def GetStatsSheet (statsInfo):
     SBVTable = GetTable(statsInfo[1]["sheet"], statsInfo[1]["sheetName"], statsInfo[1]["skiprows"], statsInfo[1]["usecols"], statsInfo[1]["nrows"])
     IDRTable = GetTable(statsInfo[2]["sheet"], statsInfo[2]["sheetName"], statsInfo[2]["skiprows"], statsInfo[2]["usecols"], statsInfo[2]["nrows"])
     
+    SBVTable.rename(columns={"Player Name": "Name", "Team(s)": "Team", "Conf." : "Conference"}, inplace=True)
     IDRTable.rename(columns={"Team": "Current Team"}, inplace=True)
+    
     mergedTable = (
             RPVTable
             .merge(SBVTable, on=["Name"])
@@ -181,84 +241,20 @@ def AddDataFrameAsTable (dataFrame, workSheet, tableName, startRow = 1, startCol
     if (tableName not in workSheet.tables):
         workSheet.add_table(Table(displayName=tableName, ref=GetExcelRange(startRow, startCol, dataFrame)))
 
-Tiers = [
-    "Premier",
-    "Master",
-    "Elite",
-    "Veteran",
-    "Rival",
-    "Challenger",
-    "Prospect",
-    "Contender",
-    "Amateur",
-]
-
-schedulePages = ScheduleSheet.sheet_names
-
-MatchDayScheduleSheetName = schedulePages[0]
-
-print("Extracting Match Day Schedules...")
-
-TierInfo = []
-TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[1], "skiprowsMatch": 5, "usecolsMatch": "B:I", "nrowsMatch": 49 - 6,       "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 49-14, "preseasonDays": 3, "matchDays": 16, "playoffDays": 2, "spacing": 4, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
-TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[2], "skiprowsMatch": 56, "usecolsMatch": "B:K", "nrowsMatch": 102 - 57,    "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 49-14,  "preseasonDays": 3,"matchDays": 17, "playoffDays": 2,"spacing": 4,  "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
-TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[3], "skiprowsMatch": 109, "usecolsMatch": "B:Q", "nrowsMatch": 158 - 110,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 87-14, "preseasonDays": 3, "matchDays": 18, "playoffDays": 3, "spacing": 5, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
-TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[4], "skiprowsMatch": 167, "usecolsMatch": "B:V", "nrowsMatch": 218 - 168,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 87-14, "preseasonDays": 3, "matchDays": 19, "playoffDays": 3, "spacing": 5, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": True})
-TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[5], "skiprowsMatch": 227, "usecolsMatch": "B:V", "nrowsMatch": 278 - 228,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 87-14, "preseasonDays": 3, "matchDays": 19, "playoffDays": 3, "spacing": 5, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": True})
-TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[6], "skiprowsMatch": 285, "usecolsMatch": "B:Q", "nrowsMatch": 334 - 286,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 87-14, "preseasonDays": 3, "matchDays": 18, "playoffDays": 3, "spacing": 5, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
-TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[7], "skiprowsMatch": 341, "usecolsMatch": "B:K", "nrowsMatch": 387 - 342,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 49-14, "preseasonDays": 3, "matchDays": 17, "playoffDays": 2, "spacing": 4, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
-TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[8], "skiprowsMatch": 394, "usecolsMatch": "B:H", "nrowsMatch": 438 - 395,  "skiprowsScrim": 13, "usecolsScrim": "B:L", "nrowsScrim": 49-14, "preseasonDays": 3, "matchDays": 16, "playoffDays": 3, "spacing": 4, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": True, "hasDivisions": False})
-TierInfo.append({"matchSheetName": schedulePages[0], "scrimSheetName" : schedulePages[9], "skiprowsMatch": 445, "usecolsMatch": "B:K", "nrowsMatch": 464 - 446,  "skiprowsScrim": 13, "usecolsScrim": "B:k", "nrowsScrim": 30-14, "preseasonDays": 3, "matchDays": 15, "playoffDays": 1, "spacing": 4, "matchDaysScrim": 16, "spacingScrim" : 2, "hasLunarConference": False, "hasDivisions": False})
-
-StatsInfo = []
-StatsInfo.append({"sheet" : RPVSheet, "sheetName": "Sorted RPV", "skiprows": 4, "usecols": "A:AD", "nrows": 848})
-StatsInfo.append({"sheet" : SBVSheet,"sheetName": "Sorted Player Stats", "skiprows": 0, "usecols": "D:AQ", "nrows": 848})
-StatsInfo.append({"sheet" : IDRSheet,"sheetName": "IDR View", "skiprows": 4, "usecols": "A:AQ", "nrows": 848})
-
-TeamsInfoCoords = { "skiprows": 0, "usecols": "D:AM", "nrows": 183 }
-
-MatchScheduleSheets = {}
-ScrimScheduleSheets = {}
-
-for i in range(len(Tiers)):
-    MatchScheduleSheets[Tiers[i]] = LinearizeMatchDaySchedule(ScheduleSheet, TierInfo[i])
-    
-    divisionOffset = 3 if TierInfo[i]["hasDivisions"] else 0
-    conferenceMultiple = 2 if TierInfo[i]["hasLunarConference"] else 1
-    
-    teamColumnNums = len(GetTable(ScheduleSheet, TierInfo[i]["matchSheetName"], TierInfo[i]["skiprowsMatch"], TierInfo[i]["usecolsMatch"], TierInfo[i]["nrowsMatch"]).columns)
-    
-    TierInfo[i]["teamNum"] = (teamColumnNums - 2 - divisionOffset) * conferenceMultiple
-    TierInfo[i]["scrimRows"] = int(TierInfo[i]["teamNum"] / TeamsPerRow) + 1
-    
-    ScrimScheduleSheets[Tiers[i]] = LinearizeScrimDaySchedule(ScheduleSheet, TierInfo[i])
-
-print("Match Day Schedules Extracted Successfully!")
-
-print("Extracting Stats Data...")
-
-StatsSheet = GetStatsSheet(StatsInfo)
-
-print("Stats Data Extracted Successfully!")
-
-# Extract all Teams Info
-print("Extracting Teams Info...")
-
-TeamInfoTable = SBVSheet.parse("Sorted Team Stats", skiprows=TeamsInfoCoords["skiprows"], usecols=TeamsInfoCoords["usecols"], nrows=TeamsInfoCoords["nrows"])
-
-print("Teams Info Extracted Successfully!")
-
-print(StatsSheet)
-
-print(TeamInfoTable)
-
-
-def sanitize_filename(filename, replacement="_"):
+def SanitizeFileName(filename, replacement="_"):
     # Define invalid characters for Windows file names
     invalid_chars = r'[\\/:*?"<>|]'
     # Replace them with the given replacement character (default: "_")
     return re.sub(invalid_chars, replacement, filename)
 
+def GetTeamSheet (teamInfo):
+    TeamSheet = teamInfo["sheet"].parse(teamInfo["sheetName"], skiprows=teamInfo["skiprows"], usecols=teamInfo["usecols"], nrows=teamInfo["nrows"])
+    TeamSheet.rename(columns={"Conf": "Conference"}, inplace=True)
+    return TeamSheet
+
+#
+# Obsidian File Generation Functions
+#
 
 def GenerateObsidianTierFiles ():
     
@@ -269,7 +265,7 @@ def GenerateObsidianTierFiles ():
     for i in range(len(Tiers)):
         Tier = Tiers[i]
         
-        obsidianFileName = sanitize_filename(Tier) + ".md"
+        obsidianFileName = SanitizeFileName(Tier) + ".md"
         
         obsidianFileContent = "---\n"
         obsidianFileContent += f"Tier: {Tier}\n"
@@ -295,7 +291,7 @@ def GenerateObsidianFranchiseFiles ():
         links.append(teamsOnFranchise["Tier"].values)
         links.append(teamsOnFranchise["Team"].values)
         
-        obsidianFileName = sanitize_filename(franchise) + ".md"
+        obsidianFileName = SanitizeFileName(franchise) + ".md"
         
         obsidianFileContent = "---\n"
         obsidianFileContent += f"Franchise: {franchise}\n"
@@ -357,15 +353,17 @@ def GenerateObsidianTeamFiles ():
     "opp_G/G": "Opponent Goals per Game"
 }
     
+    print(TeamInfoTable)
+    
     for index, teamRow in TeamInfoTable.iterrows():
         team = teamRow["Team"]
         
-        obsidianFileName = sanitize_filename(team) + ".md"
+        obsidianFileName = SanitizeFileName(team) + ".md"
         
         obsidianFileContent = "---\n"
         
         for item in linkItems:
-            obsidianFileContent += f"{item}: [[{teamRow[item].replace("\n", "")}]]\n"
+            obsidianFileContent += f"{item}: \"[[{teamRow[item].replace("\n", "")}]]\"\n"
         
         for item in nonLinkItems:
             obsidianFileContent += f"{item}: {teamRow[item]}\n"
@@ -380,11 +378,11 @@ def GenerateObsidianTeamFiles ():
 
 def GenerateObsidianPlayerFiles ():
     
-    unlinkedItems = ["Name", "RSC ID", "Tier", "Conference", "SBV", "IDR", "RPV"]
+    unlinkedItems = ["Name", "RSC ID", "Tier", "SBV", "IDR", "RPV"]
     linkedItems = ["Tier"]
     
     stat_labels = {
-    "WP": "Win Percentage",
+    "Win%": "WinPercentage",
     "W": "Wins",
     "L": "Losses",
     "P": "Points",
@@ -393,8 +391,8 @@ def GenerateObsidianPlayerFiles ():
     "G": "Goals",
     "A": "Assists",
     "Sv": "Saves",
-    "S": "Shots",
-    "S%": "Shot Percentage",
+    "Sh": "Shots",
+    "Sh%": "Shot Percentage",
     "P/A": "Points per Average",
     "SP/A": "Standard Points per Average",
     "NP/A": "Non-Standard Points per Average",
@@ -402,32 +400,41 @@ def GenerateObsidianPlayerFiles ():
     "A/A": "Assists per Average",
     "Sv/A": "Saves per Average",
     "S/A": "Shots per Average",
-    "P/G": "Points per Game",
-    "SP/G": "Standard Points per Game",
-    "NP/G": "Non-Standard Points per Game",
-    "G/G": "Goals per Game",
-    "A/G": "Assists per Game",
-    "Sv/G": "Saves per Game",
-    "S/G": "Shots per Game",
+    "PPG": "Points per Game",
+    "SPPG": "Standard Points per Game",
+    "NPPG": "Non-Standard Points per Game",
+    "GPG": "Goals per Game",
+    "APG": "Assists per Game",
+    "SvPG": "Saves per Game",
+    "ShPG": "Shots per Game",
+    "tm_G": "Team Goals",
     "opp_G": "Opponent Goals",
     "opp_G/A": "Opponent Goals per Average",
-    "opp_G/G": "Opponent Goals per Game"
-}
+    "opp_G/G": "Opponent Goals per Game",
+    "Current Team": "Team"
+    }
     
     os.makedirs(f"Output/PlayerFiles", exist_ok=True)
     
     for index, playerRow in StatsSheet.iterrows():
         
-        playerName = sanitize_filename(playerRow["Name"])
+        playerName = SanitizeFileName(playerRow["Name"])
         obsidianFileName = playerName + ".md"
         
         obsidianFileContent = "---\n"
         
-        
         obsidianFileContent += f"Teams:\n"
         
         for team in playerRow["Team"].split("\n"):
-            obsidianFileContent += f"  - \"[[{sanitize_filename(team)}]]\"\n"
+            obsidianFileContent += f"  - \"[[{SanitizeFileName(team)}]]\"\n"
+            
+        if (isinstance(playerRow["Conference"], float)):
+            obsidianFileContent += f"Conference: None\n"
+        else:
+            obsidianFileContent += f"Conference:\n"
+            
+            for team in playerRow["Conference"].split("\n"):
+                obsidianFileContent += f"  - \"{team}\"\n"
         
         for item in linkedItems:
             obsidianFileContent += f"{item}: [[{playerRow[item].replace("\n", "")}]]\n"
@@ -443,7 +450,25 @@ def GenerateObsidianPlayerFiles ():
         with open(f"Output/PlayerFiles/{obsidianFileName}", "w") as file:
             file.write(obsidianFileContent)
 
+print("Match Day Schedules Extracted Successfully!")
+
+print("Extracting Stats Data...")
+
+StatsSheet = GetStatsSheet(StatsInfo)
+
+print("Stats Data Extracted Successfully!")
+print("Extracting Teams Info...")
+
+TeamInfoTable = GetTeamSheet(TeamsInfoCoords)
+
+print("Teams Info Extracted Successfully!")
+print(StatsSheet)
+print(StatsSheet.columns)
+print(TeamInfoTable)
+
 GenerateObsidianTierFiles()
 GenerateObsidianFranchiseFiles()
 GenerateObsidianTeamFiles()
 GenerateObsidianPlayerFiles()
+
+print("Obsidian Files Generated Successfully!")
